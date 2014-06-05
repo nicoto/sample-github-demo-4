45e87da5b971f4475c51daf323d3c304da0bc6b6
OpenShift guest <nicholas_toulouse@hotmail.com>
2014-06-04 21:06:29 -0400

    classroom example

---
+++ sample-app.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+window.alert("Hello world!");
+
+var myNum = "20000";
+
+var myStr = "JavaScript is the graphic language of the future.";
+
+var myBo = false;
+
+console.log(myNum);
+
+console.log(myStr);
+
+console.log(myBo);
